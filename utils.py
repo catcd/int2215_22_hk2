33a67d843a91b067d08815c14830a632b320e1f5
Duy-Cat Can <catcd@vnu.edu.vn>
2022-03-04 10:39:45 +0700

    change utils

--- utils.py
+++ utils.py
@@ -11,13 +11,13 @@ class Timer:
             return None
         self.start_time = time.time()
         self.job = job
-        print("[INFO] {job} started.".format(job=self.job))
+        print("[LOG] {job} started.".format(job=self.job))
 
     def stop(self):
         if self.job is None:
             return None
         elapsed_time = time.time() - self.start_time
-        print("[INFO] {job} finished in {elapsed_time:0.3f} s."
+        print("[LOG] {job} finished in {elapsed_time:0.3f} s."
               .format(job=self.job, elapsed_time=elapsed_time))
         self.job = None
 
